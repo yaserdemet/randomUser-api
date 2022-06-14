88dc3f9d466a6a7ad4657ce594420530a109ae30
yaser <ddemety372@gmail.com>
2022-06-14 17:37:48 +0300

    js done

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const button = document.querySelector("#btn");
 const innerContainer = document.querySelector(".inner-container");
+// https://randomuser.me/api
 
-button.addEventListener("click" , () => {
-    const getPersonData = async () => {
-        try {
-            const url = `https://randomuser.me/api/`;
-            let res = await fetch(url);
-            let data = await res.json();
-            console.log(data);
-            createPersonData(data);
-        }
-        catch(err) {
-            alert(err);
-
-        }
-        const createPersonData = (data) => {
-                let {first , last} = data.results[0]
-                
-       
+button.addEventListener("click", () => {
+  const getPersonData = async () => {
+    try {
+      const url = `https://randomuser.me/api`;
+      let res = await fetch(url);
+      let data = await res.json();
+      //   console.log(data);
+      createPerson(data);
+    } catch (error) {
+      alert(error);
     }
-    getPersonData();
-})
+  };
+  const createPerson = (data) => {
+    let { first, last } = data.results[0].name;
+    // let name = data.results[0];
+    // let { country } = data.results[0].location;
+    let location = data.results[0].location.country;
+    let { email, phone } = data.results[0];
+    let image = data.results[0].picture.medium;
+
+    innerContainer.innerHTML = `
+    <img class="image" src="${image}" alt="img" style="width:8rem ;" />
+            <p>
+                <i class="fa-solid fa-user"></i> Name :
+                <span class="name"> ${first}</span>
+            </p>
+            <p>
+                <i class="fa-solid fa-user"></i> LastName :
+                <span class="last-name"> ${last} </span>
+            </p>
+            <p>
+                <i class="fa-solid fa-magnifying-glass-location"></i> Location :
+                <span class="location"> ${location}</span>
+            </p>
+            <p>
+                <i class="fa-solid fa-phone-flip"></i>Phone :
+                <span class="phone"> ${phone}</span>
+            </p>
+            <p>
+                <i class="fa-solid fa-envelope"></i>Email :
+                <span class="email"> ${email}</span>
+            </p>
+    
+    `;
+  };
+  getPersonData();
+});
